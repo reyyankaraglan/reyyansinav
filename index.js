e1b9c7574efe4887f862e531789ae9d42897eb83
reyyankaraglan <167758884+reyyankaraglan@users.noreply.github.com>
2024-06-05 10:56:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let  solkapi=document.getElementById={'sol-kapi'}
+
+// let  sagkapi=document.getElementById={'sag-kapi'}
+
+// let  solagac=document.getElementById={'sol-agac'}
+
+// let  sagagac=document.getElementById={'sag-agac'}
